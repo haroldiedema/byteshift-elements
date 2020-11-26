5db225197092cb0fe7b5234dc226e74ac3323446
Harold Iedema <harold@iedema.me>
2020-11-26 21:49:02 +0100

    Removed test components

--- dist/Test/DebugElement.d.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractComponent } from '@/Component/AbstractComponent';
-export declare class HelloWorld extends AbstractComponent {
-    private myText;
-    private wordCount;
-    private onTextChanged;
-}
